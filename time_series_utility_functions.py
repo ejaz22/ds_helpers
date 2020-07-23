c3f535d42e8c4f4bf3422d027569e9102816c22d
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-07-23 13:45:19 +0530

    Update time_series_utility_functions.py

--- time_series_utility_functions.py
+++ time_series_utility_functions.py
@@ -3,29 +3,28 @@
 import numpy as np
 import pandas as pd
 
-# create lagged values
+#1 create lagged values
 def ts_lags(df, lag=1):
 
     cols = [df.shift(i) for i in range(1, lag+1)]
     cols.append(df)      # append the original as the output y onto the last column
-    df = pd.concat(cols, axis=1)
-    df.fillna(0, inplace=True)   # turn NaN to 0
+    df = pd.concat(cols,axis=1)
+    df.fillna(0, inplace=True)
     return df
     
 
-# ceated datasets for time series lstm based models
-def create_dataset(df, look_back=1):
+#2 ceated datasets for time series lstm based models
+def create_dataset(data, look_back=1):
     
     """
-    this function numpy array as input and returns two
-    numpy array output
+    this function takes numpy array as input and 
+    returns two numpy array output
     """
     
     X,Y = [], []
     i_range = len(data) - look_back - 1
 
     for i in range(0, i_range):
-        X.append(data[i:(i+look_back)])    # index can move down to len(dataset)-1
-        Y.append(data[i + look_back])      # Y is the item that skips look_back number of items
-    
+        X.append(data[i:(i+look_back)])
+        Y.append(data[i + look_back])   
     return np.array(X), np.array(Y)
