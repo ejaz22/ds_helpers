037845e9eedb4876e99aa33522c1ea8c87b3ca37
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-07-24 16:54:07 +0530

    Update time_series_utility_functions.py

--- time_series_utility_functions.py
+++ time_series_utility_functions.py
@@ -40,11 +40,14 @@ def describe_data(df):
     this function takes pandas dataframe as an argument
     and returns its analysis 
     """
+    # set pandas
+    pd.set_option('precision', 2)
+    
     # dataset dimensions
-    print("1. The dataset has {} rows and {} columns".format(df.shape[0],df.shape[1]),end='\n\n\n')
+    print("1. The dataset has {} records and {} columns".format(df.shape[0],df.shape[1]),end='\n\n\n')
     
     # describe missing, unique and data_types
-    print("2. GENERAL DESCRIPTION")
+    print("2. Attributes details are as follows")
     summ = pd.DataFrame(df.dtypes,columns=['Data_Types'])
     summ = summ.reset_index()
     summ['Columns'] = summ['index']
@@ -55,12 +58,15 @@ def describe_data(df):
     
     
     # nulls analysis
-    print("3. NULLS VALUES ANALYSIS")
+    print("3. Null analysis:")
     nulls = df.isnull().sum()
     nulls = nulls[df.isnull().sum()>0].sort_values(ascending=False)
     nulls_report = pd.concat([nulls, nulls / df.shape[0]], axis=1, keys=['Missing_Values','Missing_Ratio'])
-    print(nulls_report,end='\n\n\n')
+    if nulls_report.empty:
+        print('The dataset has no null values',end='\n\n\n')
+    else:
+        print(nulls_report,end='\n\n\n')
     
     # describe stats
-    print("4. STATISTICS")
+    print("4. Statistics")
     print(df.describe().T,end='\n\n\n')
