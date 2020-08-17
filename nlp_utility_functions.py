a59a4699eeb3ad53211540c49c56e624c488e5c4
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-08-17 21:24:52 +0530

    Update nlp_utility_functions.py

--- nlp_utility_functions.py
+++ nlp_utility_functions.py
@@ -1,7 +1,7 @@
+#2. process text - white space, special character etc
 import re
 import nltk
 
-# process text - white space, special character etc
 def norm_docs(doc):
     wp = nltk.WordPunctTokenizer()
     stop_words = nltk.corpus.stopwords.words('english')
@@ -11,3 +11,57 @@ def norm_docs(doc):
     filtered_tokens = [token for token in tokens if token not in stop_words]
     doc = ' '.join(filtered_tokens)
     return doc
+
+# 2. get consine similary between two text
+import math
+import re
+from collections import Counter
+
+def get_cosine(text1, text2):
+    WORD = re.compile(r"\w+")
+    vec1 = Counter(WORD.findall(text1))
+    vec2 = Counter(WORD.findall(text2))
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
+    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+    
+    
+ # cosine similary to compare two documents
+import math
+import re
+from collections import Counter
+import pandas as pd
+
+WORD = re.compile(r"\w+")
+
+
+def get_cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
+    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
+
+df=pd.read_csv('/content/drive/article.csv')
+df['vector1']=df['headline'].apply(lambda x: text_to_vector(x)) 
+df['vector2']=df['snippet'].apply(lambda x: text_to_vector(x)) 
+df['simscore']=df.apply(lambda x: get_cosine(x['vector1'],x['vector2']),axis=1)
