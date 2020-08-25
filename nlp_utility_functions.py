ff435e1c42b238fb112e13147658aaead1c3e95c
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-08-25 23:29:40 +0530

    Update nlp_utility_functions.py

--- nlp_utility_functions.py
+++ nlp_utility_functions.py
@@ -32,33 +32,20 @@ def get_cosine(text1, text2):
     
    
  # cosine similary to compare two documents
-import math
-import re
-from collections import Counter
-import pandas as pd
-
-WORD = re.compile(r"\w+")
-
-
-def get_cosine(vec1, vec2):
-    intersection = set(vec1.keys()) & set(vec2.keys())
-    numerator = sum([vec1[x] * vec2[x] for x in intersection])
-
-    sum1 = sum([vec1[x] ** 2 for x in list(vec1.keys())])
-    sum2 = sum([vec2[x] ** 2 for x in list(vec2.keys())])
-    denominator = math.sqrt(sum1) * math.sqrt(sum2)
-
-    if not denominator:
-        return 0.0
-    else:
-        return float(numerator) / denominator
-
-
-def text_to_vector(text):
-    words = WORD.findall(text)
-    return Counter(words)
 
 df=pd.read_csv('/content/drive/article.csv')
 df['vector1']=df['headline'].apply(lambda x: text_to_vector(x)) 
 df['vector2']=df['snippet'].apply(lambda x: text_to_vector(x)) 
 df['simscore']=df.apply(lambda x: get_cosine(x['vector1'],x['vector2']),axis=1)
+
+
+
+# get top 10 words
+from collections import Counter
+from itertools import chain
+
+def top_n(docs,n=10):
+    """ Returns top 10 words from a pandas series"""
+    assert docs.__class__.__name__ == 'Series', 'Input is not a pandas series'
+    words_counts = Counter(chain.from_iterable([i.split(" ") for i in docs]))
+    return sorted(words_counts.items(), key=lambda x: x[1], reverse=True)[:n]
