6c78e7aea5efb73bc3fe73f03c63cec4e67f5ec7
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-12-16 12:21:22 +0530

    Update ml_utility_functions.py

--- ml_utility_functions.py
+++ ml_utility_functions.py
@@ -1,3 +1,16 @@
+# perform TSNE
+def perform_tsne(X, y, perplexity=100, learning_rate=200, n_components=2):
+    tsne = TSNE(n_components=n_components, init='random',
+                         random_state=None, perplexity=perplexity, verbose=1)
+    result = tsne.fit_transform(X)
+    result = pd.DataFrame(result)
+    result = result.join(y)
+    result.columns = ['x0', 'x1', 'y']
+    sns.lmplot('x0', 'x1', result, fit_reg=False, hue='y', palette={0:"#2662c1", 1:"#c9001e"},
+              scatter_kws={'alpha': .5})
+    plt.title('t-SNE plot')
+    plt.plot()
+
 
 # get feature importance
 def get_rf_feat_importances(X,y):
