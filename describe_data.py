2f69618fed8ccb66d0ec5a3c5df3d58857bd50a4
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-12-16 21:09:27 +0530

    Update describe_data.py

--- describe_data.py
+++ describe_data.py
@@ -1,3 +1,91 @@
+import numpy as np
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+
+
+def display_all(df, rows=10, columns=1000):
+    """
+    :param df: Pandas DataFrame
+    :param rows: Max Number of rows to print
+    :param columns: Max Number of columns to print
+    """
+    with pd.option_context("display.max_rows", rows, "display.max_columns", columns):
+        print(df)
+
+
+def auto_set_type(df, type_dict):
+    """
+    :param df: Pandas DataFrame
+    :param type_dict: Dictionary of column name and type to format to in the format:
+            {'month': CategoricalDtype(categories=range(1,13))}
+            ** Only necessary for Categorical & Boolean Columns **
+    :return: Pandas DataFrame
+    """
+    for col in type_dict:
+        df[col] = df[col].astype(type_dict[col])
+
+    return df
+
+
+def auto_dates(df, date_name='date', time=False):
+    """
+    :param df: Pandas DataFrame
+    :param date_name: Only needed if index is not DatetimeIndex type.
+                        String: Name of date column in df
+    :param time: Set to True if time part is necessary
+    :return: Pandas DataFrame
+    """
+    T = df.index.to_series() if isinstance(df.index, pd.DatetimeIndex) else pd.to_datetime(df[date_name])
+
+    if time:
+        df['hour'] = T.dt.hour
+        df['minute'] = T.dt.minute
+        df['second'] = T.dt.second
+    df['year'] = T.dt.year
+    df['month'] = T.dt.month.astype(CategoricalDtype(categories=range(1, 13)))
+    df['week'] = T.dt.weekofyear.astype(CategoricalDtype(categories=range(1, 54)))
+    df['day'] = T.dt.day.astype(CategoricalDtype(categories=range(1, 32)))
+    df['dow'] = T.dt.dayofweek.astype(CategoricalDtype(categories=range(0, 7)))
+    df['doy'] = T.dt.dayofyear.astype(CategoricalDtype(categories=range(0, 366)))
+    df['start_month'] = T.dt.is_month_start.astype('int')
+    df['end_month'] = T.dt.is_month_end.astype('int')
+    df['start_quarter'] = T.dt.is_quarter_start.astype('int')
+    df['end_quarter'] = T.dt.is_quarter_end.astype('int')
+    df['start_year'] = T.dt.is_year_start.astype('int')
+    df['end_year'] = T.dt.is_year_end.astype('int')
+    df['n_day_in_month'] = T.dt.days_in_month
+    return df
+
+
+def auto_remove_novariance(df):
+    """
+    :param df: Pandas DataFrame
+    :return: Pandas DataFrame
+    """
+    uniques = df.apply(pd.Series.nunique)
+    return df.drop(columns=list(uniques[uniques == 1].index))
+
+
+def auto_dummies(df):
+    """
+    :param df: Pandas DataFrame
+    :return: Pandas Dataframe()
+    """
+    return pd.get_dummies(df, columns=list(df.select_dtypes(include='category').columns), drop_first=True)
+
+
+def get_category_size(df, max_cat=50):
+    cat = df.select_dtypes(include='category').columns
+    return [(c, len(df[c].cat.categories), min(max_cat, len(df[c].cat.categories) // 2)) for c in cat]
+
+
+def sort_columns(df, reverse=False):
+    return df[sorted(df.columns, reverse=reverse)]
+
+
+
+
+
 """
 This function performs several analysis on passed dataframe.
 dependency : tabulate module
