2e8c823630ce9835de6da4afbdec8cef7cc629fc
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2020-12-16 12:23:11 +0530

    Update time_series_utility_functions.py

--- time_series_utility_functions.py
+++ time_series_utility_functions.py
@@ -1,6 +1,27 @@
 import numpy as np
 import pandas as pd
 
+from statsmodels.stats.outliers_influence import variance_inflation_factor as vif
+
+# get varinace inflation factor
+def get_vif(X):
+    
+    """
+    Takes a pd.DataFrame or 2D np.array
+    and prints Variance Inflation Factor 
+    for every variable.
+    """
+    
+    if isinstance(data, pd.DataFrame) == False:
+        X = pd.DataFrame(X)
+    
+    X['__INTERCEPT'] = np.ones(X.shape[0])
+    
+    for i in range(X.shape[1]-1):
+        the_vif = vif(X.values, i)
+        print("VIF for column {:03}: {:.02f}".format(i, the_vif))
+
+
 #1 create lagged values
 def ts_lags(df, lag=1):
 
