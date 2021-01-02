e8f81a38ae37fe058b89bc4472af4a62a4e84b69
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2021-01-02 12:58:43 +0530

    Update tsa.py

--- tsa.py
+++ tsa.py
@@ -3,6 +3,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.stats.outliers_influence import variance_inflation_factor as vif
 
+
+# Plot and test stationary
+def test_stationarity(timeseries):
+    plt.figure(figsize=(50, 8))
+    #Determing rolling statistics
+    rolmean = pd.rolling_mean(timeseries, window=7)
+    rolstd = pd.rolling_std(timeseries, window=7)
+
+    #Plot rolling statistics:
+    orig = plt.plot(timeseries, color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+    
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = sm.tsa.adfuller(timeseries['Visits'], autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
+
 # frame a sequence as a supervised learning problem
 def timeseries_to_supervised(data, lag=1):
 	df = DataFrame(data)
