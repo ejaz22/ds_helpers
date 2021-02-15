80ccb132a9d51d5cc89230c7680fbc4e3a468a9f
Ejaz Ahmed <43725530+ejaz22@users.noreply.github.com>
2021-02-15 14:34:01 +0530

    Update tsa.py

--- tsa.py
+++ tsa.py
@@ -17,6 +17,39 @@ def build_temporal_features(data: pd.DataFrame,dt_var=None):
     
     return data
 
+# TS Plots
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
+
+def tsplot(y, lags=None, figsize=(10, 8), style='bmh',title=''):
+	"""
+	Plots the following
+	AR, Autocorrelation, Partial Autocorrelation,
+	QQ Plot, Probability Plot
+	"""
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):    
+        fig = plt.figure(figsize=figsize)
+        #mpl.rcParams['font.family'] = 'Ubuntu Mono'
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+        
+        y.plot(ax=ts_ax)
+        ts_ax.set_title(title)
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.5)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.5)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')        
+        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return 
 
 # Plot and test stationary
 def test_stationarity(timeseries):
